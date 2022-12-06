a1c7eb67fee423e4529e632d0335d00bd7f80a71
Guilherme <contatoguigvd@hotmail.com>
2022-12-05 23:36:10 -0300

    first commit

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+	method: "GET",
+	headers: {
+	  "X-RapidAPI-Key": "294d9b7d5emsh763e81989b70a23p1c215djsne742a59fc28b",
+	  "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+	},
+  };
+  export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+  export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+
+  export const WEATHER_API_KEY = "c8e83dd8de58410835d2956e85bd08e4"
